d0f92064afc22edb57e7500f74d9e593d201f2ec
Sharon T <sharonmctsai@gmail.com>
2025-01-19 14:28:19 +0000

    userdashboard page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
+import UserDashboard from './components/UserDashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
 
       </Routes>
     </Router>
---
+++ src/components/UserDashboard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function UserDashboard() {
+  return (
+    <div style={styles.container}>
+      <h2>User interface</h2>
+      <p>to be continue。</p>
+    
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px',
+  },
+};
+
+export default UserDashboard;
